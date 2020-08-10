c7be764814c5af2b994e765355602d5657737890
niyazimuhammetkose <mxready@windowslive.com>
2020-08-11 01:52:54 +0300

    categoryadmin

--- Book/admin.py
+++ Book/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from Book.models import Category, Book
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'publisher', 'price', 'amount', 'status']
+    list_filter = ['title', 'author', 'category', 'publisher', 'status']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Book,BookAdmin)
