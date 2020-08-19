dafc335778988f2022a33a3d5210ee63e6e89c2b
niyazimuhammetkose <mxready@windowslive.com>
2020-08-19 23:04:29 +0300

    userpanel

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'Book.apps.BookConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
